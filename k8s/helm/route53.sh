dbf2cc811b9ded4045d8ff45cfb265c66081d56c
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-04-29 21:17:51 +0200

    Update route53.sh

--- k8s/helm/route53.sh
+++ k8s/helm/route53.sh
@@ -6,16 +6,13 @@ VAR=$(pulumi stack output kinesisFirehoseRole -s bee7ch7/experiments/dev)
 echo $VAR
 
 RESULT=$(aws elbv2 describe-load-balancers --name aws --query LoadBalancers[0].CreatedTime --output text)
-ALB_CREATED=${RESULT:0:10}
-ALB_CREATED2=$(date -d ${RESULT:0:10} +%s)
+GET_DATE=${RESULT:0:10}
 
+ALB_CREATED=$(date -d ${GET_DATE:0:10} +%s)
 TODAY=$(date +%F)
-TODAY2=$(date -d $TODAY +%s)
 
-echo $ALB_CREATED
-echo $TODAY
-echo $ALB_CREATED2 $TODAY2 $TODAY
-if [[ "$ALB_CREATED2" -lt "$TODAY" ]]
+echo $ALB_CREATED $TODAY
+if [ $ALB_CREATED -ne $TODAY ]
 then
   echo "Nothing to do"
   exit 0
