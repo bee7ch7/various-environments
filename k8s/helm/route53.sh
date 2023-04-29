e6e2e6d5a2d6651a63f6ef07723846e346740fad
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-04-29 20:35:52 +0200

    Update route53.sh

--- k8s/helm/route53.sh
+++ k8s/helm/route53.sh
@@ -4,3 +4,15 @@
 VAR=$(pulumi stack output kinesisFirehoseRole -s bee7ch7/experiments/dev)
 
 echo $VAR
+
+RESULT=$(aws elbv2 describe-load-balancers --name aws --query LoadBalancers[0].CreatedTime --output text)
+ALB_CREATED=${RESULT:0:10}
+
+TODAY=$(date +%F)
+echo $ALB_CREATED
+echo TODAY
+if [[ "$ALB_CREATED" < "$TODAY" ]]
+then
+  echo "RUN ROUTE53 UPDATE"
+else
+  echo NOTHING TO DO
