bbd887df5075d265abf68c39ae10ddd138dcf48d
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-04-29 21:12:23 +0200

    Update route53.sh

--- k8s/helm/route53.sh
+++ k8s/helm/route53.sh
@@ -7,11 +7,13 @@ echo $VAR
 
 RESULT=$(aws elbv2 describe-load-balancers --name aws --query LoadBalancers[0].CreatedTime --output text)
 ALB_CREATED=${RESULT:0:10}
-
+ALB_CREATED2=$(date -d ${RESULT:0:10} +%s)
+TODAY2=$(date -d $TODAY +%s)
 TODAY=$(date +%F)
 echo $ALB_CREATED
 echo $TODAY
-if [[ "$ALB_CREATED" -lt "$TODAY" ]]
+echo $ALB_CREATED2 $TODAY2
+if [[ "$ALB_CREATED2" -lt "$TODAY2" ]]
 then
   echo "Nothing to do"
   exit 0
