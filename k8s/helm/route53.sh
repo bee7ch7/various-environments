b538216807f61284dd55fb358aff2010006ccbe4
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-04-30 14:55:44 +0200

    Update route53.sh

--- k8s/helm/route53.sh
+++ k8s/helm/route53.sh
@@ -5,7 +5,7 @@ NOW=$(date +%F)
 GET_ALB=$(aws elbv2 describe-load-balancers --name aws --query LoadBalancers[0].CreatedTime --output text)
 
 ALB_CREATED=$(date -d ${GET_ALB:0:10} +%s) # ${GET_DATE:0:10}
-TODAY=$(date -d $NOW +%s)
+TODAY=$(date -d "2023-04-29" +%s) # TODAY=$(date -d $NOW +%s)
 
 echo $ALB_CREATED $TODAY
 if [ "$ALB_CREATED" -lt "$TODAY" ]
