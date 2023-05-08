83318f05fdbe1bccd24cd0a5b4e284a136b520df
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2023-05-08 10:51:00 +0200

    docs: tests

--- create_pr.sh
+++ create_pr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ci tests
+# ci tests #
 if [ $(git rev-parse --abbrev-ref HEAD) == 'main' ]; then gh pr create --base dev --head main --title 'docs: [skip ci] backmerge' --body '[skip ci] backmerge' ; fi
